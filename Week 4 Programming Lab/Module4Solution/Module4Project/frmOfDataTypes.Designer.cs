a2d71e2a521f7bd8d50e4b35d5dae817c2dde4cd
ShadowKnight-15 <eric_christensen15@hotmail.com>
2025-11-23 14:19:25 -0600

    Week 4 Programming Lab

---
+++ Week 4 Programming Lab/Module4Solution/Module4Project/frmOfDataTypes.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Module4Project
+{
+    partial class frmOfDataTypes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnByte = new System.Windows.Forms.Button();
+            this.btnShort = new System.Windows.Forms.Button();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.btnLong = new System.Windows.Forms.Button();
+            this.btnFloat = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.btnDecimal = new System.Windows.Forms.Button();
+            this.btnPow = new System.Windows.Forms.Button();
+            this.btnRound = new System.Windows.Forms.Button();
+            this.btnSqrt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnByte
+            // 
+            this.btnByte.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnByte.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnByte.ForeColor = System.Drawing.Color.Yellow;
+            this.btnByte.Location = new System.Drawing.Point(42, 341);
+            this.btnByte.Name = "btnByte";
+            this.btnByte.Size = new System.Drawing.Size(98, 50);
+            this.btnByte.TabIndex = 0;
+            this.btnByte.Text = "&Byte";
+            this.btnByte.UseVisualStyleBackColor = false;
+            this.btnByte.Click += new System.EventHandler(this.btnByte_Click);
+            // 
+            // btnShort
+            // 
+            this.btnShort.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnShort.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShort.ForeColor = System.Drawing.Color.Yellow;
+            this.btnShort.Location = new System.Drawing.Point(146, 341);
+            this.btnShort.Name = "btnShort";
+            this.btnShort.Size = new System.Drawing.Size(98, 50);
+            this.btnShort.TabIndex = 1;
+            this.btnShort.Text = "&Short";
+            this.btnShort.UseVisualStyleBackColor = false;
+            this.btnShort.Click += new System.EventHandler(this.btnShort_Click);
+            // 
+            // btnInt
+            // 
+            this.btnInt.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnInt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInt.ForeColor = System.Drawing.Color.Yellow;
+            this.btnInt.Location = new System.Drawing.Point(250, 341);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(98, 50);
+            this.btnInt.TabIndex = 2;
+            this.btnInt.Text = "&Int";
+            this.btnInt.UseVisualStyleBackColor = false;
+            this.btnInt.Click += new System.EventHandler(this.btnInt_Click);
+            // 
+            // btnLong
+            // 
+            this.btnLong.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnLong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLong.ForeColor = System.Drawing.Color.Yellow;
+            this.btnLong.Location = new System.Drawing.Point(354, 341);
+            this.btnLong.Name = "btnLong";
+            this.btnLong.Size = new System.Drawing.Size(98, 50);
+            this.btnLong.TabIndex = 3;
+            this.btnLong.Text = "&Long";
+            this.btnLong.UseVisualStyleBackColor = false;
+            this.btnLong.Click += new System.EventHandler(this.btnLong_Click);
+            // 
+            // btnFloat
+            // 
+            this.btnFloat.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnFloat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFloat.ForeColor = System.Drawing.Color.Yellow;
+            this.btnFloat.Location = new System.Drawing.Point(146, 397);
+            this.btnFloat.Name = "btnFloat";
+            this.btnFloat.Size = new System.Drawing.Size(98, 50);
+            this.btnFloat.TabIndex = 4;
+            this.btnFloat.Text = "&Float";
+            this.btnFloat.UseVisualStyleBackColor = false;
+            this.btnFloat.Click += new System.EventHandler(this.btnFloat_Click);
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnDouble.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDouble.ForeColor = System.Drawing.Color.Yellow;
+            this.btnDouble.Location = new System.Drawing.Point(250, 397);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(98, 50);
+            this.btnDouble.TabIndex = 5;
+            this.btnDouble.Text = "&Double";
+            this.btnDouble.UseVisualStyleBackColor = false;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
+            // 
+            // btnDecimal
+            // 
+            this.btnDecimal.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnDecimal.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecimal.ForeColor = System.Drawing.Color.Yellow;
+            this.btnDecimal.Location = new System.Drawing.Point(354, 397);
+            this.btnDecimal.Name = "btnDecimal";
+            this.btnDecimal.Size = new System.Drawing.Size(98, 50);
+            this.btnDecimal.TabIndex = 6;
+            this.btnDecimal.Text = "D&ecimal";
+            this.btnDecimal.UseVisualStyleBackColor = false;
+            this.btnDecimal.Click += new System.EventHandler(this.btnDecimal_Click);
+            // 
+            // btnPow
+            // 
+            this.btnPow.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnPow.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPow.ForeColor = System.Drawing.Color.DarkViolet;
+            this.btnPow.Location = new System.Drawing.Point(146, 453);
+            this.btnPow.Name = "btnPow";
+            this.btnPow.Size = new System.Drawing.Size(98, 50);
+            this.btnPow.TabIndex = 7;
+            this.btnPow.Text = "&Pow";
+            this.btnPow.UseVisualStyleBackColor = false;
+            this.btnPow.Click += new System.EventHandler(this.btnPow_Click);
+            // 
+            // btnRound
+            // 
+            this.btnRound.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnRound.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRound.ForeColor = System.Drawing.Color.DarkViolet;
+            this.btnRound.Location = new System.Drawing.Point(250, 453);
+            this.btnRound.Name = "btnRound";
+            this.btnRound.Size = new System.Drawing.Size(98, 50);
+            this.btnRound.TabIndex = 8;
+            this.btnRound.Text = "&Round";
+            this.btnRound.UseVisualStyleBackColor = false;
+            this.btnRound.Click += new System.EventHandler(this.btnRound_Click);
+            // 
+            // btnSqrt
+            // 
+            this.btnSqrt.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSqrt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSqrt.ForeColor = System.Drawing.Color.DarkViolet;
+            this.btnSqrt.Location = new System.Drawing.Point(354, 453);
+            this.btnSqrt.Name = "btnSqrt";
+            this.btnSqrt.Size = new System.Drawing.Size(98, 50);
+            this.btnSqrt.TabIndex = 9;
+            this.btnSqrt.Text = "S&qrt";
+            this.btnSqrt.UseVisualStyleBackColor = false;
+            this.btnSqrt.Click += new System.EventHandler(this.btnSqrt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Yellow;
+            this.btnClear.Location = new System.Drawing.Point(42, 397);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(98, 50);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Yellow;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.OrangeRed;
+            this.btnExit.Location = new System.Drawing.Point(42, 453);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(98, 50);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BackColor = System.Drawing.Color.Bisque;
+            this.lblDisplay.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(66, 269);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(375, 60);
+            this.lblDisplay.TabIndex = 12;
+            this.lblDisplay.Text = "Click a Button";
+            this.lblDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmOfDataTypes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 537);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSqrt);
+            this.Controls.Add(this.btnRound);
+            this.Controls.Add(this.btnPow);
+            this.Controls.Add(this.btnDecimal);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnFloat);
+            this.Controls.Add(this.btnLong);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.btnShort);
+            this.Controls.Add(this.btnByte);
+            this.Name = "frmOfDataTypes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Week 4 - Data Types";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnByte;
+        private System.Windows.Forms.Button btnShort;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Button btnLong;
+        private System.Windows.Forms.Button btnFloat;
+        private System.Windows.Forms.Button btnDouble;
+        private System.Windows.Forms.Button btnDecimal;
+        private System.Windows.Forms.Button btnPow;
+        private System.Windows.Forms.Button btnRound;
+        private System.Windows.Forms.Button btnSqrt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblDisplay;
+    }
+}
+
---
+++ Week 4 Programming Lab/Module4Solution/Module4Project/frmOfDataTypes.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* 
+ Name: Eric Christensen
+Class: ITD 2343 - Obj-Orient Prg Using C#
+Due Date: 10/4/25
+ */
+
+namespace Module4Project
+{
+    public partial class frmOfDataTypes : Form
+    {
+        public frmOfDataTypes()
+        {
+            InitializeComponent();
+        }
+
+        //Clear and Exit buttons
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "";
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Integer Buttons
+
+        private void btnByte_Click(object sender, EventArgs e)
+        {
+            byte leftOperand = 2, rightOperand = 3;
+            byte result = (byte)(leftOperand + rightOperand);
+
+            lblDisplay.Text = String.Format("{0} + {1} = {2}", leftOperand, rightOperand, result);
+        }
+        private void btnShort_Click(object sender, EventArgs e)
+        {
+            short leftOperand = 10, rightOperand = 4;
+            short result = (short)(leftOperand - rightOperand);
+
+            lblDisplay.Text = String.Format("{0} - {1} = {2}", leftOperand, rightOperand, result);
+        }
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            int leftOperand = 7, rightOperand = 2;
+            int result = leftOperand / rightOperand;
+
+            lblDisplay.Text = String.Format("{0} / {1} = {2}", leftOperand, rightOperand, result);
+        }
+        private void btnLong_Click(object sender, EventArgs e)
+        {
+            long leftOperand = 25, rightOperand = 4;
+            long result = leftOperand % rightOperand;
+
+            lblDisplay.Text = String.Format("{0} % {1} = {2}", leftOperand, rightOperand, result);
+        }
+
+        //Decimal Buttons
+
+        private void btnFloat_Click(object sender, EventArgs e)
+        {
+            float leftOperand = 7.1234567f, rightOperand = 2.0f;
+            float result = leftOperand % rightOperand;
+
+            lblDisplay.Text = String.Format("{0} % {1} = {2:F7}", leftOperand, rightOperand, result);
+        }
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            double leftOperand = 22.0, rightOperand = 7.0;
+            double result = leftOperand / rightOperand;
+
+            lblDisplay.Text = String.Format("{0} / {1} = {2:F15}", leftOperand, rightOperand, result);
+        }
+        private void btnDecimal_Click(object sender, EventArgs e)
+        {
+            decimal leftOperand = 3.1415926535897932384626433832m;
+            decimal rightOperand = 2.5m;
+            decimal result = leftOperand * rightOperand;
+
+            lblDisplay.Text = String.Format("{0} * {1} = {2:F28}", leftOperand, rightOperand, result);
+        }
+
+        //Math Operations
+
+        private void btnPow_Click(object sender, EventArgs e)
+        {
+            double baseNum = 2.0, exponent = 3.0;
+            double result = Math.Pow(baseNum, exponent);
+
+            lblDisplay.Text = String.Format("{0} raised to {1} = {2}", baseNum, exponent, result);
+        }
+        private void btnRound_Click(object sender, EventArgs e)
+        {
+            double number = 3.14159;
+            int decimals = 2; // must be int
+            double result = Math.Round(number, decimals);
+
+            lblDisplay.Text = String.Format("Rounding {0} to {1} decimals = {2}", number, decimals, result);
+        }
+        private void btnSqrt_Click(object sender, EventArgs e)
+        {
+            double number = 16.0;
+            double result = Math.Sqrt(number);
+
+            lblDisplay.Text = String.Format("The square root of {0} is {1}", number, result);
+        }
+    }
+}
