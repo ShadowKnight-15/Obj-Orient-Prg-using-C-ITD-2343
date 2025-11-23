e0b601a6d1e356f3b122d38c3266e3469e5221c2
ShadowKnight-15 <eric_christensen15@hotmail.com>
2025-11-23 14:19:51 -0600

    Week 5 Programming Lab

---
+++ Week 5 - Programming Lab/Solution5Christensen/Project5Christensen/ComparisonClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5Christensen
+{
+    internal class ComparisonClass
+    {
+        public double LeftValue { get; set; }
+        public double RightValue { get; set; }
+        public double CheckValue { get; set; }
+
+        public ComparisonClass()
+        {
+
+        }
+        public ComparisonClass(double left, double right, double check)
+        {
+            LeftValue = left;
+            RightValue = right;
+            CheckValue = check;
+        }
+        public string GetResultMessage()
+        {
+            if (CheckValue == LeftValue)
+            {
+                return "The number equals the lower bound.";
+            }
+
+            if (CheckValue == RightValue)
+            {
+                return "The number equals the upper bound.";
+            }
+
+            if (CheckValue < LeftValue)
+            {
+                return "The number is less than the lower bound.";
+            }
+
+            if (CheckValue > RightValue)
+            {
+                return "The number is greater than the upper bound.";
+            }
+
+            return "The number is between the lower and upper bounds.";
+        }
+    
+    }
+}
---
+++ Week 5 - Programming Lab/Solution5Christensen/Project5Christensen/frmChoices.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* 
+ Name: Eric Christensen
+Class: ITD 2343 - Object Oriented Programming with C#
+Due date: 10/7/25
+ */
+
+namespace Project5Christensen
+{
+    public partial class frmChoices : Form
+    {
+        public frmChoices()
+        {
+            InitializeComponent();
+
+            //Check and exit Button
+
+            this.AcceptButton = btnCheck;
+
+            this.CancelButton = btnExit;
+
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLeftNum.Text = "";
+            txtRightNum.Text = "";
+            txtNumChecked.Text = "";
+            lblMsgToUser.Text = "Inputs cleared. Enter new values to continue.";
+        }
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            // Parse inputs
+            int low = int.Parse(txtLeftNum.Text);
+            int high = int.Parse(txtRightNum.Text);
+            int check = int.Parse(txtNumChecked.Text);
+
+            // Compare check value with the range
+            if (check == low)
+            {
+                // Equal to low end
+                lblMsgToUser.Text = "The number is equal to the Low End of the Range.";
+            }
+            else if (check == high)
+            {
+                // Equal to high end
+                lblMsgToUser.Text = "The number is equal to the High End of the Range.";
+            }
+            else if (check < low)
+            {
+                // Below the low end
+                lblMsgToUser.Text = "The number is less than the Low End of the Range.";
+            }
+            else if (check > high)
+            {
+                // Above the high end
+                lblMsgToUser.Text = "The number is greater than the High End of the Range.";
+            }
+            else
+            {
+                // Within the range
+                lblMsgToUser.Text = $"The number is in the range of {low} and {high}.";
+            }
+
+            // Clear the check textbox and return focus for new input
+            txtNumChecked.Text = "";
+            txtNumChecked.Focus();
+        }
+    }
+}
