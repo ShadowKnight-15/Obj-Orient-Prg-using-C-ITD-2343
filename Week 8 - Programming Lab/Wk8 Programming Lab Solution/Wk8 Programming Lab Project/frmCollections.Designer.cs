266f230aff7d8703945a226bfd0538a06e79f01d
ShadowKnight-15 <eric_christensen15@hotmail.com>
2025-11-23 14:21:13 -0600

    Week 8 Programming Lab

---
+++ Week 8 - Programming Lab/Wk8 Programming Lab Solution/Wk8 Programming Lab Project/frmCollections.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Wk8_Programming_Lab_Project
+{
+    partial class frmCollections
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtEnteredNum = new System.Windows.Forms.TextBox();
+            this.lblEntryNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Gold;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(623, 130);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(146, 56);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Gold;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(623, 192);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(146, 56);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            // 
+            // txtEnteredNum
+            // 
+            this.txtEnteredNum.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnteredNum.Location = new System.Drawing.Point(154, 75);
+            this.txtEnteredNum.Name = "txtEnteredNum";
+            this.txtEnteredNum.Size = new System.Drawing.Size(199, 34);
+            this.txtEnteredNum.TabIndex = 2;
+            // 
+            // lblEntryNum
+            // 
+            this.lblEntryNum.AutoSize = true;
+            this.lblEntryNum.BackColor = System.Drawing.Color.LawnGreen;
+            this.lblEntryNum.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEntryNum.Location = new System.Drawing.Point(91, 50);
+            this.lblEntryNum.Name = "lblEntryNum";
+            this.lblEntryNum.Size = new System.Drawing.Size(335, 22);
+            this.lblEntryNum.TabIndex = 3;
+            this.lblEntryNum.Text = "Enter a Number between -1217 and 1217";
+            // 
+            // frmCollections
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEntryNum);
+            this.Controls.Add(this.txtEnteredNum);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmCollections";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Collection Lab";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtEnteredNum;
+        private System.Windows.Forms.Label lblEntryNum;
+    }
+}
+
---
+++ Week 8 - Programming Lab/Wk8 Programming Lab Solution/Wk8 Programming Lab Project/frmCollections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wk8_Programming_Lab_Project
+{
+    public partial class frmCollections : Form
+    {
+        public frmCollections()
+        {
+            InitializeComponent();
+        }
+    }
+}
